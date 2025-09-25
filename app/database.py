caff1cd884b1a698974dd8fd05e65079792e95d2
Enn-Kayy <nk0030802@gmail.com>
2025-09-25 16:27:11 +0530

    Add File

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+## this should match our alembic.ini file
+DATABASE_URL = "postgresql://postgres:Naman%402002@localhost/fastapi_users"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency to get a DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
